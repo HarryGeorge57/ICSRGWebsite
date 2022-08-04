f582b4c04cfee64bcd3e5b20c6145db556ffe2f4
Harry George <george.harry.thomas@gmail.com>
2022-08-04 13:52:45 +0100

    fixed heroku deploy issue. Will now branch

--- index.js
+++ index.js
@@ -7,21 +7,10 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 8080;
 
+
 //use body parser middleware
 app.use(bodyParser.urlencoded({exended: false}));
 
-
-//API KEY pk_6b72cb1fb7e44a8c8563dcea04dd5943
-//create call api function
-function call_api(finishedAPI, ticker) {
-    request('https://cloud.iexapis.com/stable/stock/'+ ticker +'/quote?token=pk_6b72cb1fb7e44a8c8563dcea04dd5943', {json: true}, (err, res, body) => {
-        if(err){return console.log(err);}
-        if(res.statusCode === 200){
-            // console.log(body);
-            finishedAPI(body);
-        };
-    });
-}
 //Set Handlebars Middleware
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
@@ -29,18 +18,9 @@ app.set('view engine', 'handlebars');
 
 //Set handelbar index GET routes
 app.get('/', function (req, res){
-    call_api(function(doneAPI){
-            res.render('home')});
-});
-
-
-//Set handelbar index POST routes
-app.post('/', function (req, res){
-    call_api(function(doneAPI){
-            //posted_stuff = req.body.stock_ticker;
             res.render('home');
-    });
 });
+
 
 //Create About Page Route
 app.get('/about.html', function (req, res){
