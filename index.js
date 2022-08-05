1a450e049d8b58b332079b055e92c1acfb48952f
Harry George <george.harry.thomas@gmail.com>
2022-08-05 10:12:37 +0100

    removed api, created text file to store api in

--- index.js
+++ index.js
@@ -4,52 +4,12 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 const request = require('request');
 const bodyParser = require('body-parser');
+const sync = require('sync');
 
 const PORT = process.env.PORT || 8080;
 
+
 //iRacing Driver Data Importing
-const fetch = require('node-fetch'); //this may be outdated with the newest node.js, but plenty of docs exist for including other files
-﻿var loginCookies = '';
-
-
-sync function getAuth()
-{
-    console.log('AUTH')
-    const accessReply = await fetch('https://members-ng.iracing.com/auth', {
-        method: 'POST',
-        body:   JSON.stringify(
-        {
-            "email": iRacingEmailforauth,
-            "password": iRacingPassword
-        }),
-        credentials: 'include',
-        headers: {'Accept': '*/*', "Content-type": "application/json"}
-    })
-    const replyBody = await accessReply.json();
-    var statusCode = await accessReply.status;
-    console.log(stringTime() + "Auth status code: " + statusCode)
-    if(statusCode == 200)
-    {
-        delay = originalDelay;
-    }
-    if(statusCode == 503)
-    {
-        delay = maintenceDelay;
-    }
-    loginCookies = parseCookies(accessReply);
-    return statusCode;
-}
-
-
-function parseCookies(response) 
-{
-    const raw = response.headers.raw()['set-cookie'];
-    return raw.map((entry) => {
-      const parts = entry.split(';');
-      const cookiePart = parts[0];
-      return cookiePart;
-    }).join(';');
-}
 
 //use body parser middleware
 app.use(bodyParser.urlencoded({exended: false}));
@@ -61,8 +21,11 @@ app.set('view engine', 'handlebars');
 
 //Set handelbar index GET routes
 app.get('/', function (req, res){
-            res.render('home');
-});
+        getCarName(function(){
+            res.render('home', { 
+            });
+        });
+    });
 
 
 //Page Routing
