187b86f612096e6ae088b48ad2f8a0e92cd7de74
Harry George <george.harry.thomas@gmail.com>
2022-08-05 09:40:25 +0100

    code for getting iRacing data added

--- index.js
+++ index.js
@@ -8,7 +8,48 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 
 //iRacing Driver Data Importing
-// function call_iRacing()
+const fetch = require('node-fetch'); //this may be outdated with the newest node.js, but plenty of docs exist for including other files
+﻿var loginCookies = '';
+
+
+sync function getAuth()
+{
+    console.log('AUTH')
+    const accessReply = await fetch('https://members-ng.iracing.com/auth', {
+        method: 'POST',
+        body:   JSON.stringify(
+        {
+            "email": iRacingEmailforauth,
+            "password": iRacingPassword
+        }),
+        credentials: 'include',
+        headers: {'Accept': '*/*', "Content-type": "application/json"}
+    })
+    const replyBody = await accessReply.json();
+    var statusCode = await accessReply.status;
+    console.log(stringTime() + "Auth status code: " + statusCode)
+    if(statusCode == 200)
+    {
+        delay = originalDelay;
+    }
+    if(statusCode == 503)
+    {
+        delay = maintenceDelay;
+    }
+    loginCookies = parseCookies(accessReply);
+    return statusCode;
+}
+
+
+function parseCookies(response) 
+{
+    const raw = response.headers.raw()['set-cookie'];
+    return raw.map((entry) => {
+      const parts = entry.split(';');
+      const cookiePart = parts[0];
+      return cookiePart;
+    }).join(';');
+}
 
 //use body parser middleware
 app.use(bodyParser.urlencoded({exended: false}));
